06b09158e8a806c6d169f1ba8f2a9676c296347f
Alexander Foster <afoster009@hotmail.ca>
2017-09-18 22:23:46 +0000

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,26 @@
+#First we will define our Dance Module.
+
+module Dance
+  # Next we will give the dance module a series of methods
+
+  def twirl
+    "I'm twirling!"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+   "I'm doing a pirouette"
+ end
+
+ def take_a_bow
+   "Thank you, thank you. It was a pleasure to dance for you all."
+ end
+
+
+
+end
+
+# Next we will define our Kid class and tell it to include the methods of the dance module. lib/kid.rb
--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,21 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+# to lend our two classes all the methods within the Dance module we use the 'include' keyword.
+class Dancer
+  #include Dance
+  extend MetaDancing
+  include FancyDance::ClassMethods # refactored way.
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# If we use the include keyword, we allow our classes to use all of the mthods included in the module as instance methods
+#
+# Now that we have included the module open up bin/dance_party.
